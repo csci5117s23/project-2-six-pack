932ab9451902de7a6a34c34f5d714b8d57966300
Andrew <andrew.j.owens1@gmail.com>
2023-04-04 19:15:07 -0500

    sample page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,56 @@
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { FC, useState } from "react";
+import { useRef } from "react";
+
+const Navbar = ({}) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return(<>
+        <nav className="bg-black-100">
+            <div className="max-w-6xl mx-auto px-4">
+            <div className="flex justify-between">
+
+                <div className="flex space-x-4">
+                
+                <div>
+                    <a href="#" className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="2" width="20" height="20" rx="2.18" ry="2.18"></rect><line x1="7" y1="2" x2="7" y2="22"></line><line x1="17" y1="2" x2="17" y2="22"></line><line x1="2" y1="12" x2="22" y2="12"></line><line x1="2" y1="7" x2="7" y2="7"></line><line x1="2" y1="17" x2="7" y2="17"></line><line x1="17" y1="17" x2="22" y2="17"></line><line x1="17" y1="7" x2="22" y2="7"></line></svg>
+                    <span className="font-bold text-white px-2">ScreenStash</span>
+                    </a>
+                </div>
+                </div>
+
+            
+                <div className="hidden md:flex items-center space-x-1">
+                    <a href="" className="py-5 px-3 text-white-700 hover:text-gray-300">Search</a>
+                <a href="" className="py-5 px-3">Login</a>
+                <a href="" className="py-2 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300">Signup</a>
+                </div>
+
+            
+                <div className="md:hidden flex items-center">
+                <button className="mobile-menu-button" onClick={toggleMenu}>
+                    <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+                </div>
+            
+            </div>
+            </div>
+
+        {/* Menu in mobile view */}
+        <div className={`mobile-menu ${isMenuOpen ? 'block' : 'hidden'} md:hidden`}>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">Features</a>
+          <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">Pricing</a>
+        </div>
+      </nav>
+    </>)
+}
+
+export default Navbar
---
+++ src/pages/movieDetails.tsx
@@ -0,0 +1,5 @@
+export default function movieDetails() {
+    return(<>
+    <div>Hello, World!</div>
+    </>)
+}
