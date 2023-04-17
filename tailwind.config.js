acc2f2ec9b6d3b6d64d742b5a807e6967778359e
cavag004 <96396906+cavag004@users.noreply.github.com>
2023-04-17 16:55:54 -0500

    Switched dashboard and index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,22 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        // 'hero-pattern': "url('https://i.ibb.co/Qn5BR8N/bg.png')",
+        'hero-pattern': "/pics/Frame 1.png"
+      }),
+      animation: {
+        'ltr-linear-infinite': 'move-bg 5s linear infinite',
+      },
+      keyframes: {
+        'move-bg': {
+          '0%': { 'background-position': '0 0' },
+          '100%': { 'background-position': '256px 0' },
+        },
+      },
+    },
   },
+  variants: {},
   plugins: [],
 }
-
