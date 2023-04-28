6a99c4f0e06b1566a1d3e47c7b9c2689633adfdb
TheAndrew-O <andrew.j.owens1@gmail.com>
2023-04-28 15:39:55 -0500

    fixed grid-layout

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -65,12 +65,10 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
           {/* EITHER WAY, THIS SHOULD BE THE GENERAL LAYOUT, REPLACE THE INNER DIV WITH MOVIE API COMPONENT */}
           <div className="grid place-items-start justify-items-center grid-flow-row overflow-y-auto mx-2 movie_list_h">
               <div className="grid gap-4 grid-cols-2 md:grid-cols-8 lg:gap-6 mt-1">
-                  <ul>
                       {/* Ensure that movielist is array and then iterate if so */}
                       {Array.isArray(movies) && movies.map((movie) => {
                           return (
-                              <li key={movie._id} className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded">
-                                  Here
+                              <div key={movie._id} className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded">
                                   <img
                                       className="rounded h-auto max-w-full transition-transform duration-500 group-hover:scale-125"
                                       src={`https://image.tmdb.org/t/p/original${movie.posterImageUrlPath}`} alt=""/>
@@ -78,8 +76,8 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
                                       className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
                                       {/* TODO: ADD PREVIEW DETAILS */}
                                       <div
-                                          className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                                          PLACE DETAILS HERE, EITHER DESCRIPTION OR WHERE TO WATCH
+                                          className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 text-ellipsis overflow-hidden">
+                                          {movie.description}
                                       </div>
                                       <Link href={{
                                           pathname: "/movieDetails/",
@@ -89,14 +87,14 @@ const Modal = ({ isOpen, onClose }: ModalProps) => {
                                           }
                                       }}
                                           as={`movieDetails/${movie.title}`}
-                                          className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60">
+                                          className="rounded-full bg-neutral-900 py-1 px-1.5 text-sm capitalize text-white shadow shadow-black/60">
                                           See More
                                       </Link>
                                   </div>
 
-                              </li>)
+                              </div>)
                       })}
-                  </ul>
+                  
                   {/* <div className="rounded h-64 md:h-80 h-auto w-32 md:w-48 md:max-h-80 bg-red-500">asda</div>
                   <div className="rounded h-64 md:h-80 h-auto w-32 md:w-48 md:max-h-80 bg-green-500">asda</div>
                   <div className="rounded h-64 md:h-80 h-auto w-32 md:w-48 md:max-h-80 bg-yellow-500">asda</div>
