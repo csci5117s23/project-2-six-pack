01e6c195a3afc153401ccf82d28600ea7d4c450c
TheAndrew-O <andrew.j.owens1@gmail.com>
2023-04-30 13:07:49 -0500

    added add and delete

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -7,7 +7,7 @@ import {useMovieListContext} from "@/components/MovieListContext";
 // When user adds movie from Modal, display added movies in MovieList
 const MovieList = () => {
     const {movieList, movieListMovies} = useMovieListContext();
-
+    const {deleteMovieFromMovieList} = useMovieListContext();
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     return (<>
@@ -23,31 +23,41 @@ const MovieList = () => {
         {movieListMovies.length === 0 &&
             <div className="justify-center text-center inter text-slate-200">Add Something To Get Started!</div>}
 
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-3 lg:grid-cols-5 justify-center px-2">
-            {movieList?.name}
-            <ul>
+            <div
+                className="grid place-items-start justify-items-center grid-flow-row overflow-y-auto mx-2 movie_list_h">
+                <div className="grid gap-4 grid-cols-2 md:grid-cols-8 lg:gap-6 mt-1">
                 {movieListMovies.map((movie) => {
                     return (
-                        <li className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded">
+                        <div key={movie._id}
+                             className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl rounded bg-black">
                             {movie.posterImageUrlPath && <img
                                 className="rounded h-auto max-w-full transition-transform duration-500 group-hover:scale-125"
-                                src={`https://image.tmdb.org/t/p/original${movie.posterImageUrlPath}`} alt=""/>}
+                                src={`https://image.tmdb.org/t/p/original${movie.posterImageUrlPath}`}
+                                alt=""/>}
                             <div
-                                className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+                                className="absolute inset-0 flex translate-y-[100%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0 text-ellipsis bg-black/60">
                                 {/* TODO: ADD PREVIEW DETAILS */}
                                 <div
-                                    className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                                    PLACE DETAILS HERE, EITHER DESCRIPTION OR WHERE TO WATCH
+                                    className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 overflow-hidden rounded">
+                                    {movie.title}
+                                    <div>{movie.releaseDate.substring(0, 4)}</div>
                                 </div>
-                                <Link href={`movieDetails/${movie._id}`}
-                                      className="rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60">
-                                    See More
-                                </Link>
+                                <div className='flex'>
+                                    <Link href={`movieDetails/${movie._id}`}
+                                          className="rounded-full bg-neutral-900 py-1 px-1.5 text-sm capitalize text-white">
+                                        See More
+                                    </Link>
+                                    <button
+                                        className="rounded-full bg-red-500 py-1 px-1.5 text-sm capitalize text-white"
+                                        onClick={async () => await deleteMovieFromMovieList(movie)}>
+                                        Delete
+                                    </button>
+                                </div>
                             </div>
 
-                        </li>)
+                        </div>)
                 })}
-            </ul>
+                </div>
         </div>
     </>)
 }
